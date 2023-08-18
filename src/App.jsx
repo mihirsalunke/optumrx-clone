6f7e318cc475286e1986efe23d2f0ef65eb4923e
Mihir Salunke <salunke.mi@husky.neu.edu>
2023-08-17 18:47:38 -0700

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
+import { Router, Route, Routes } from "@solidjs/router";
+import Home from "./Pages/Home";
+import DrugPricing from "./Pages/DrugPricing";
+import MedicationList from "./Pages/MedicationList";
+import OrderBuilder from "./Pages/OrderBuilder";
+import OrderConfirmation from "./Pages/OrderConfirmation";
+import OrderStatus from "./Pages/OrderStatus";
+import PharmacyLocator from "./Pages/PharmacyLocator";
+import PrescriptionDrugList from "./Pages/PrescriptionDrugList";
+import SavingsCenter from "./Pages/SavingsCenter";
+
 function App() {
   return (
-    <p class="text-7xl text-orange-700 text-center py-20">Optum-Liveperson demo!</p>
+    <Router>
+      <Routes>
+        <Route path="/" component={Home} />
+        <Route path="/secure/drug-pricing" component={DrugPricing} />
+        <Route path="/secure/medication-list" component={MedicationList} />
+        <Route path="/secure/order-builder" component={OrderBuilder} />
+        <Route path="/secure/order-confirmation" component={OrderConfirmation} />
+        <Route path="/secure/order-status" component={OrderStatus} />
+        <Route path="/secure/pharmacy-locator" component={PharmacyLocator} />
+        <Route path="/secure/prescription-drug-list" component={PrescriptionDrugList} />
+        <Route path="/secure/saving-center" component={SavingsCenter} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Pages/index.js
@@ -0,0 +1,11 @@
+import { Home } from './Home';
+import { DrugPricing } from './DrugPricing';
+import { MedicationList } from './MedicationList';
+import { OrderBuilder } from './OrderBuilder';
+import { OrderConfirmation } from './OrderConfirmation';
+import { OrderStatus } from './OrderStatus';
+import { PharmacyLocator } from './PharmacyLocator';
+import { PrescriptionDrugList } from './PrescriptionDrugList';
+import { SavingsCenter } from './SavingsCenter';
+
+export { Home, DrugPricing, MedicationList, OrderBuilder, OrderConfirmation, OrderStatus, PharmacyLocator, PrescriptionDrugList, SavingsCenter };
